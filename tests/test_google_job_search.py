447d4888828e034acda87264d19b310d84808936
Javan Moe <javan.moe@mindlinksoft.com>
2023-04-29 13:39:36 +0100

    add command tp disable gpu and extensions

--- tests/test_google_job_search.py
+++ tests/test_google_job_search.py
@@ -22,6 +22,8 @@ def setup():
     chrome_options = ChromeOptions()
     # chrome_options.add_argument('--headless')
     chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     driver = EventFiringWebDriver(webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options),
                                   EventListener())
