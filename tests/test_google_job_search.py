88ee4dacc049562b944998ad586c65f72ab6588b
Javan Moe <javan.moe@mindlinksoft.com>
2023-04-29 13:57:20 +0100

    removing headless

--- tests/test_google_job_search.py
+++ tests/test_google_job_search.py
@@ -20,7 +20,7 @@ LANGUAGES = []
 def setup():
     global driver
     chrome_options = ChromeOptions()
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-gpu")
